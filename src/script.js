8588a187c8124b49c72f3dc6b7d90747f94e65dc
Nusrat Jahan <njnusrat53@gmail.com>
2020-07-27 22:37:17 +0600

    function added

--- src/script.js
+++ src/script.js
@@ -6,23 +6,22 @@ const calcDisplay = document.querySelector('#calc-display');
 const matched = document.querySelector('.match');
 const notMatched = document.querySelector('.not-match');
 const submitBtn = document.querySelector('.submit-btn');
-const inputField = document.querySelectorAll('.form-control');
 const countTry = document.querySelector('#try-count');
+const inputField = document.querySelectorAll('.form-control');
 
-/* Generate PIN Number and show it in input field */
+/* HIDE NOTIFY MESSAGE */
+display('none', 'none');
+
+/* PIN GENERATE AND DISPLAY IN INPUT FIELD */
 generateBtn.addEventListener('click', function () {
 	let min = 1000;
 	let max = 9999;
 	displayPin.value = Math.floor(Math.random() * (max - min + 1)) + min; //random number generate
-	removeSignal();
-	display('none', 'none');
-	submitBtn.removeAttribute('disabled')
-	count = 3;
-	countTry.innerHTML = ` ${count} try left`;
-	calcDisplay.value = ''
+	submitBtn.removeAttribute('disabled');
+	renderInfo();
 });
 
-/* Show the button number to calculator screen */
+/* USER INPUT AND SHOW IN CALC SCREEN */
 btnNumber.forEach((button) => {
 	button.addEventListener('click', function (event) {
 		if (!event.target.classList.contains('delete', 'clear')) {
@@ -30,27 +29,30 @@ btnNumber.forEach((button) => {
 			calcDisplay.value += parseInt(button.innerHTML);
 			countTry.innerHTML = ` ${count} try left`;
 			removeSignal();
+			display('none', 'none');
 		}
 	});
 });
 
-/* Submit Button functionality */
+/* SUBMIT BUTTON AND VALIDATION */
 let count = 3;
 submitBtn.addEventListener('click', function () {
 	if (displayPin.value === '') {
 		//check if pin input value is empty!
 		alert('Please Generate a PIN Number!!');
 		countTry.innerText = countTry.innerText;
-	} else if (calcDisplay.value === displayPin.value) {
+	}
+	else if (calcDisplay.value === displayPin.value) {
 		//check if pin value and calculator input value is equal
-		display('none', 'block');
+		display('none', 'block'); //notify matched message
 		inputField.forEach((signal) => {
 			signal.classList.add('green-signal');
 		});
 		countTry.innerHTML = '<b>SUCCESS</b>';
-	} else {
+	}
+	else {
 		//if pin value and calc value is not equal
-		display('block', 'none');
+		display('block', 'none'); //notify not matched message
 		inputField.forEach((signal) => {
 			signal.classList.add('red-signal');
 		});
@@ -58,43 +60,51 @@ submitBtn.addEventListener('click', function () {
 		countTry.innerHTML = `${count} try left`;
 		if (count === 0) {
 			//disable submit button after 3 try
-			submitBtn.setAttribute('disabled', true)
+			submitBtn.setAttribute('disabled', true);
 		}
 	}
 });
 
-/* Backspace button */
+/* DELETE BUTTON */
 document.querySelector('.delete').addEventListener('click', function () {
 	calcDisplay.value = calcDisplay.value.slice(0, calcDisplay.value.length - 1);
 	display('none', 'none');
 	removeSignal();
 });
 
-/* Clear button (clear all given input) */
+/* ALL CLEAR BUTTON */
 document.querySelector('.clear').addEventListener('click', function () {
 	calcDisplay.value = '';
 	removeSignal();
 	display('none', 'none');
 });
 
-/* Disabling the input focus written */
-inputField.forEach((input) => {
-	input.addEventListener('input', function () {
-		input.value = '';
-		alert('You are not allowed to give input manually');
-	});
+/* ALERT IF USER MANUALLY WRITE PIN */
+document.querySelector('.form-control').addEventListener('input', function () {
+	displayPin.value = '';
+	alert('You are not allowed to give input manually');
+	renderInfo();
 });
 
-//FUNCTIONS
-/* input field red green signal functionality */
-function removeSignal() {
+/* ----------FUNCTIONS------------ */
+
+/* input field red green signal function */
+function removeSignal () {
 	inputField.forEach((signal) => {
 		signal.classList.remove('green-signal', 'red-signal');
 	});
 }
 
-/* match,not-match function */
-function display(firstDisplay, secondDisplay) {
+/* notify function */
+function display (firstDisplay, secondDisplay) {
 	notMatched.style.display = firstDisplay;
 	matched.style.display = secondDisplay;
+}
+
+function renderInfo () {
+	display('none', 'none');
+	calcDisplay.value = '';
+	removeSignal();
+	count = 3;
+	countTry.innerHTML = ` ${count} try left`;
 }
