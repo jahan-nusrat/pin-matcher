ae05e65e8abdaefc4ad96598b8bcc5465f3f5eff
Nusrat Jahan <njnusrat53@gmail.com>
2020-07-27 20:52:39 +0600

    final commit

--- src/script.js
+++ src/script.js
@@ -1,65 +1,62 @@
 /* Variable assign */
-const generatePinBtn = document.querySelector('.generate-btn');
-const showPin = document.getElementById('pin-show');
+const generateBtn = document.querySelector('.generate-btn');
+const displayPin = document.getElementById('pin-display');
 const btnNumber = document.querySelectorAll('.button');
-const calcScreen = document.querySelector('#calc-screen');
-const correct = document.querySelector('.match');
-const wrong = document.querySelector('.not-match');
+const calcDisplay = document.querySelector('#calc-display');
+const matched = document.querySelector('.match');
+const notMatched = document.querySelector('.not-match');
 const submitBtn = document.querySelector('.submit-btn');
 const inputField = document.querySelectorAll('.form-control');
-const value = document.querySelector('#value');
-const text = document.querySelector('#text');
+const countTry = document.querySelector('#try-count');
 
 /* Generate PIN Number and show it in input field */
-generatePinBtn.addEventListener('click', function () {
+generateBtn.addEventListener('click', function () {
 	let min = 1000;
 	let max = 9999;
-	showPin.value = Math.floor(Math.random() * (max - min + 1)) + min; //random number generate
-	removeSignal()
-	display()
+	displayPin.value = Math.floor(Math.random() * (max - min + 1)) + min; //random number generate
+	removeSignal();
+	countTry.innerHTML = ` ${count} try left`;
+	display('none', 'none');
 });
 
 /* Show the button number to calculator screen */
 btnNumber.forEach((button) => {
 	button.addEventListener('click', function (event) {
-		if (!event.target.classList.contains('delete', 'clear')) { //check if target is not clear and backspace button
-			calcScreen.value += parseInt(button.innerHTML);
-			display()
-			removeSignal()
+		if (!event.target.classList.contains('delete', 'clear')) {
+			//check if target is not clear and backspace button
+			calcDisplay.value += parseInt(button.innerHTML);
+			countTry.innerHTML = ` ${count} try left`;
+			removeSignal();
 		}
 	});
-});
-
-/* Clear button (clear all given input) */
-document.querySelector('.clear').addEventListener('click', function () {
-	calcScreen.value = '';
-	removeSignal()
 });
 
 /* Submit Button functionality */
 let count = 3;
 submitBtn.addEventListener('click', function () {
-	if (showPin.value === '') { //check if pin input value is empty!
-		alert('Please Generate a PIN Number!!')
-		text.innerText = text.innerText
-
-	} else if (calcScreen.value === showPin.value) { //check if pin value and calculator input value is equal
-		correct.style.display = 'block';
-		wrong.style.display = 'none';
+	if (displayPin.value === '') {
+		//check if pin input value is empty!
+		alert('Please Generate a PIN Number!!');
+		countTry.innerText = countTry.innerText;
+	}
+	else if (calcDisplay.value === displayPin.value) {
+		//check if pin value and calculator input value is equal
+		display('none', 'block');
 		inputField.forEach((signal) => {
 			signal.classList.add('green-signal');
 		});
-		text.innerHTML = '<b>SUCCESS</b>';
-
-	} else { //if pin value and calc value is not equal
-		wrong.style.display = 'block';
-		correct.style.display = 'none';
+		countTry.innerHTML = '<b>SUCCESS</b>';
+	}
+	else {
+		//if pin value and calc value is not equal
+		display('block', 'none');
 		inputField.forEach((signal) => {
 			signal.classList.add('red-signal');
 		});
-		count--
-		text.innerHTML = `${count} try left`
-		if (count === 0) { //disable submit button after 3 try
+		count--;
+		countTry.innerHTML = `${count} try left`;
+		if (count === 0) {
+			//disable submit button after 3 try
 			submitBtn.style.display = 'none';
 		}
 	}
@@ -75,21 +72,28 @@ inputField.forEach((input) => {
 
 /* Backspace button */
 document.querySelector('.delete').addEventListener('click', function () {
-	calcScreen.value = calcScreen.value.slice(0, calcScreen.value.length - 1);
-	wrong.style.display = 'none';
-	correct.style.display = 'none';
-	removeSignal()
+	calcDisplay.value = calcDisplay.value.slice(0, calcDisplay.value.length - 1);
+	display('none', 'none');
+	removeSignal();
 });
 
+/* Clear button (clear all given input) */
+document.querySelector('.clear').addEventListener('click', function () {
+	calcDisplay.value = '';
+	removeSignal();
+	display('none', 'none');
+});
 
-function removeSignal() {
+//FUNCTIONS
+/* input field red green signal functionality */
+function removeSignal () {
 	inputField.forEach((signal) => {
 		signal.classList.remove('green-signal', 'red-signal');
 	});
 }
 
-function display() {
-	wrong.style.display = 'none';
-	correct.style.display = 'none';
-	text.innerHTML = ` ${count} try left`
+/* match,not-match function */
+function display (firstDisplay, secondDisplay) {
+	notMatched.style.display = firstDisplay;
+	matched.style.display = secondDisplay;
 }
